9f31c94c4d6af0792b6f14b5f3005572d0ebaf43
alberttiny <1257236262@qq.com>
2020-02-24 15:03:26 +0800

    init

---
+++ sharding-jdbc-table/src/main/java/com/albert/shardingjdbctable/ShardingJdbcTableApplication.java
@@ -0,0 +1,13 @@
+package com.albert.shardingjdbctable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShardingJdbcTableApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShardingJdbcTableApplication.class, args);
+    }
+
+}
---
+++ sharding-jdbc-table/src/test/java/com/albert/shardingjdbctable/ShardingJdbcTableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.albert.shardingjdbctable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShardingJdbcTableApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
